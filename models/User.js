739c0e73071fdea06f5251512f33e4240363c10d
Jad Taha <jadtaha.de@gmail.com>
2022-06-11 22:52:01 +0200

    userSchema  --verison 01.00.86

--- models/User.js
+++ models/User.js
@@ -1,3 +1,18 @@
 import mongoose from 'mongoose';
 import { isEmail } from 'validator';
 import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Please enter an Email'],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid Email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter an Email'],
+    minlength: [6, 'Minimum password length is a 6 charachtars'],
+  },
+});
