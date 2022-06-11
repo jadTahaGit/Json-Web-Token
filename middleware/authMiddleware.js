37cca0ed43ee33b777608de11de0fe8ab0717172
Jad Taha <jadtaha.de@gmail.com>
2022-06-11 23:08:53 +0200

    authMiddlware  --verison 01.01.81

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,47 @@
+const JWT = require('jsonwebtoken');
+const User = require('../models/User');
+
+const requireAuth = (req, res, next) => {
+  const token = req.cookies.access_token;
+
+  // check json web token exists & is verified
+  if (token) {
+    JWT.verify(token, 'FedJadPedKarAli2022', (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.redirect('/login');
+      } else {
+        console.log(decodedToken);
+        next();
+      }
+    });
+  } else {
+    res.redirect('/login');
+  }
+};
+
+// check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (token) {
+    JWT.verify(token, 'FedJadPedKarAli2022', async (err, decodedToken) => {
+      if (err) {
+        console.log('error verifying the token');
+        console.log(err.message);
+        res.locals.user = null;
+        next();
+      } else {
+        console.log(decodedToken);
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    console.log('no token');
+    res.locals.user = null;
+    next();
+  }
+};
+
+module.exports = { requireAuth, checkUser };
