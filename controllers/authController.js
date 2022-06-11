7eee5c823f329e1663c8d145225f5ba24fea8be7
Jad Taha <jadtaha.de@gmail.com>
2022-06-11 22:57:12 +0200

    signup_post --verison 01.00.95

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,3 +4,27 @@ const JWT = require('jsonwebtoken');
 module.exports.signup_get = (req, res) => {
   res.render('signup');
 };
+
+module.exports.login_get = (req, res) => {
+  res.render('login');
+};
+
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.create({
+      email,
+      password,
+    });
+    const token = createToken(user._id);
+    console.log(token);
+    res.cookie('access_token', token, {
+      httpOnly: true,
+      maxAge: maxAge * 1000,
+    });
+    res.status(201).json({ user: user._id });
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
+};
