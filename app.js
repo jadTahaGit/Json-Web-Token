0769dc6e9221e53a1665e2935bf09113ef1b681e
Jad Taha <jadtaha.de@gmail.com>
2022-06-11 20:34:40 +0200

    DB connection  --verison 01.00.16

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI =
+  'mongodb+srv://jadtaha:8JlOTrtdfH1u@cluster0.imrf7.mongodb.net/node-auth?';
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => {
+    console.log('DB connected Successfully');
+    console.log('Listining on Server 4000');
+    app.listen(4000);
+  })
+  .catch((err) => console.log(err));
