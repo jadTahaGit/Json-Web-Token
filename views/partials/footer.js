0bf0c6a0ebe3e7792216f61a0d789f100c1affe7
Jad Taha <jadtaha.de@gmail.com>
2022-06-11 20:41:16 +0200

    HomePage  --verison 01.00.30

---
+++ views/partials/footer.js
@@ -0,0 +1,11 @@
+<%- include('partials/header'); -%>
+
+<header>
+    <div className="headings">
+        <h2>Naruto Movements</h2>
+        <h3>By Jad Taha</h2>
+        <a href="/movements" class="btn"> View Movements</a>
+    </div>
+</header>
+
+<%- include('partials/footer'); -%>
