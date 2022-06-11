9429c491e6dd6882026142d6d2195845a9ec6210
Jad Taha <jadtaha.de@gmail.com>
2022-06-11 23:01:43 +0200

    createToken --verison 01.01.06

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,6 +27,7 @@ const handleErrors = (err) => {
   return errors;
 };
 
+// create Token
 const maxAge = 3 * 24 * 60 * 60;
 const createToken = (id) => {
   return JWT.sign({ id }, 'FedJadPedKarAli2022', {
